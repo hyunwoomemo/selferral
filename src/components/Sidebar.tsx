d9a51dabc5e8453c19b4e068c689d5cdc4406802
leehyunwoo <archihw94@gmail.com>
2024-06-26 15:49:46 +0900

    feat: 사이드바 링크 적용

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,9 +6,18 @@ import Button from "./common/Button";
 import { styles } from "@/styles";
 import Image from "next/image";
 import { IoIosArrowForward } from "react-icons/io";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 const Sidebar = () => {
   const [sidebar, setSidebar] = useAtom(sidebarAtom);
+
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setSidebar("-300px");
+  }, [pathname, setSidebar]);
 
   return (
     <Container sidebar={sidebar}>
@@ -17,22 +26,22 @@ const Sidebar = () => {
         <Button label="회원가입" bgc={styles.blue} onClick={() => null} small borderRadius={10} />
       </Wrapper>
       <SidebarItemWrapper>
-        <SidebarItem>
+        <SidebarItem href={"/"}>
           <SidebarItemIcon src={require("@/assets/home.webp")} alt="icon" />
           <SidebarItemName>홈</SidebarItemName>
           <IoIosArrowForward color={styles.grayText} />
         </SidebarItem>
-        <SidebarItem>
+        <SidebarItem href={"/affiliated"}>
           <SidebarItemIcon src={require("@/assets/exchange.webp")} alt="icon" />
           <SidebarItemName>제휴 거래소</SidebarItemName>
           <IoIosArrowForward color={styles.grayText} />
         </SidebarItem>
-        <SidebarItem>
+        <SidebarItem href="/">
           <SidebarItemIcon src={require("@/assets/introduce.webp")} alt="icon" />
           <SidebarItemName>서비스 소개</SidebarItemName>
           <IoIosArrowForward color={styles.grayText} />
         </SidebarItem>
-        <SidebarItem>
+        <SidebarItem href="/payback">
           <SidebarItemIcon src={require("@/assets/payback.webp")} alt="icon" />
           <SidebarItemName>페이백 테스트</SidebarItemName>
           <IoIosArrowForward color={styles.grayText} />
@@ -69,10 +78,12 @@ const SidebarItemWrapper = styled.div`
   padding-top: 50px;
 `;
 
-const SidebarItem = styled.div`
+const SidebarItem = styled(Link)`
   display: flex;
   gap: 10px;
   align-items: center;
+  text-decoration: none;
+  color: #000;
 `;
 
 const SidebarItemIcon = styled(Image)<{ src: string }>`
