f5bd7eface3371c445717a667e36b96b024d9560
leehyunwoo <archihw94@gmail.com>
2024-07-05 15:06:48 +0900

    수정

--- src/app/affiliated/trade-list.tsx
+++ src/app/affiliated/trade-list.tsx
@@ -2,12 +2,16 @@
 import { dummyTrade } from "@/dummy";
 import styled from "styled-components";
 import TradeItem from "./trade-item";
+import { useRouter } from 'next/navigation';
 
 const TradeList = () => {
+
+  const router = useRouter();
+
   return (
     <Container>
       {dummyTrade.map((dummy, index) => {
-        return <TradeItem key={index} data={dummy} />;
+        return <TradeItem onClick={() => router.push(`/exchandeDetail/${dummy.name.toLowerCase()}`)} key={index} data={dummy} />;
       })}
     </Container>
   );
