92440328056576d6b4b0989f3b045c5bee9587b6
leehyunwoo <archihw94@gmail.com>
2024-07-05 17:24:51 +0900

    풀페이지 라이브러리 적용

--- app/payback/process/page1.tsx
+++ app/payback/process/page1.tsx
@@ -3,7 +3,7 @@ import { dummyTrade } from "@/dummy";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
-const Page1 = () => {
+const Page1 = ({ onClick }: { onClick: () => void }) => {
   return (
     <div className="px-2 py-5 text-xl font-bold">
       <div>가장 자주 쓰는</div>
@@ -12,7 +12,7 @@ const Page1 = () => {
 
       <div className="flex flex-wrap gap-5 py-10 justify-center">
         {dummyTrade.map((v) => (
-          <div className="min-w-[47%] flex items-center py-2 gap-2" key={v.name}>
+          <div onClick={onClick} className="min-w-[47%] flex items-center py-2 gap-2" key={v.name}>
             <div className="w-10 h-10 relative">
               <Image src={v.image} alt="image" />
             </div>
@@ -21,14 +21,14 @@ const Page1 = () => {
         ))}
       </div>
 
-      <div className="flex gap-4 w-full justify-center">
+      {/* <div className="flex gap-4 w-full justify-center">
         <Button className={cn(buttonVariants({ variant: "outline", size: "lg" }), "border border-orange-400 dark:border-orange-200 text-orange-400 dark:text-orange-200 w-fit px-16")}>
           <p>이전</p>
         </Button>
         <Button className={cn(buttonVariants({ variant: "outline", size: "lg" }), "border border-orange-400 dark:border-orange-200 text-orange-400 dark:text-orange-200 w-fit px-16")}>
           <p>다음</p>
         </Button>
-      </div>
+      </div> */}
     </div>
   );
 };
