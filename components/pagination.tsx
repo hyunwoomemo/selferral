9bc980c4a2d5eed60aa57004231371a1947b5a63
leehyunwoo <archihw94@gmail.com>
2024-09-29 16:31:22 +0900

    수정

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -7,7 +7,11 @@ const Pagination = ({ page = 1, total, offset = 10, setPage }) => {
       {Array(Math.ceil(total / offset))
         .fill("")
         .map((v, i) => (
-          <div onClick={() => setPage(i + 1)} className={cn("w-10 h-10 flex justify-center items-center cursor-pointer rounded-full", page === i + 1 ? "bg-orange-400 text-white" : "bg-gray-100")}>
+          <div
+            key={i}
+            onClick={() => setPage(i + 1)}
+            className={cn("w-10 h-10 flex justify-center items-center cursor-pointer rounded-full", page === i + 1 ? "bg-orange-400 text-white" : "bg-gray-100")}
+          >
             {i + 1}
           </div>
         ))}
