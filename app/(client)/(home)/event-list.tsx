51b82799df4bdd123008fb5213eded690565a741
leehyunwoo <archihw94@gmail.com>
2024-10-25 15:04:50 +0900

    수정

--- app/(client)/(home)/event-list.tsx
+++ app/(client)/(home)/event-list.tsx
@@ -18,26 +18,28 @@ const EventList = ({ data }) => {
         }}
         className="mySwiper"
       >
-        {data.map((v, index) => (
-          <SwiperSlide id="event-slide" about="" key={index}>
-            <div className="w-full max-w-[155px] md:max-w-[200px] cursor-pointer" key={index} onClick={() => v.link && v.link != "null" && window.open(v.link)}>
-              <Image style={{ borderRadius: 20 }} width={200} height={180} src={`http://api.xn--3l2b13oekp.com${v.path}`} className="object-cover w-full max-h-[180px]" alt="eventImage" />
-              {/* <div className="relative w-full"> */}
-              {/* <Image className="rounded-lg object-cover" src={dummy.image} fill alt="eventImage" /> */}
-              {/* <div className="relative"> */}
-              {/* </div> */}
-              {/* </div> */}
-              <p className="text-sm w-full font-bold text-gray-400  pt-3">{v.title}</p>
-              <p className="text-sm md:text-[16px] w-full py-1  font-bold break-keep">{v.memo}</p>
-              <div className="flex gap-1 text-[10px] md:text-xs text-gray-600 dark:text-gray-200">
-                <CalendarDaysIcon size={16} />
-                <p>{moment(v.starttime).format("YYYY-MM-DD")}</p>
-                <p>~</p>
-                <p>{moment(v.endtime).format("YYYY-MM-DD")}</p>
+        {data
+          .sort((a, b) => new Date(a.endtime) - new Date(b.endtime))
+          .map((v, index) => (
+            <SwiperSlide id="event-slide" about="" key={index}>
+              <div className="w-full max-w-[155px] md:max-w-[200px] cursor-pointer" key={index} onClick={() => v.link && v.link != "null" && window.open(v.link)}>
+                <Image style={{ borderRadius: 20 }} width={200} height={180} src={`http://api.xn--3l2b13oekp.com${v.path}`} className="object-cover w-full max-h-[180px]" alt="eventImage" />
+                {/* <div className="relative w-full"> */}
+                {/* <Image className="rounded-lg object-cover" src={dummy.image} fill alt="eventImage" /> */}
+                {/* <div className="relative"> */}
+                {/* </div> */}
+                {/* </div> */}
+                <p className="text-sm w-full font-bold text-gray-400  pt-3">{v.title}</p>
+                <p className="text-sm md:text-[16px] w-full py-1  font-bold break-keep">{v.memo}</p>
+                <div className="flex gap-1 text-[10px] md:text-xs text-gray-600 dark:text-gray-200">
+                  <CalendarDaysIcon size={16} />
+                  <p>{moment(v.starttime).format("YYYY-MM-DD")}</p>
+                  <p>~</p>
+                  <p>{moment(v.endtime).format("YYYY-MM-DD")}</p>
+                </div>
               </div>
-            </div>
-          </SwiperSlide>
-        ))}
+            </SwiperSlide>
+          ))}
       </Swiper>
     </div>
   );
