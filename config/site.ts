4db9a56c98567aab3238b493fe32b0cf4bd52570
leehyunwoo <archihw94@gmail.com>
2025-03-26 11:21:21 +0900

    test

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "seleferral.com",
   url: "https://example.com",
-  description: "selferral 소개 문구",
+  description: "selferral 소개 문구1",
   author: "selferral",
   copyright: "copyright © 2024 selferral, All Right Reserved",
   // links: {
